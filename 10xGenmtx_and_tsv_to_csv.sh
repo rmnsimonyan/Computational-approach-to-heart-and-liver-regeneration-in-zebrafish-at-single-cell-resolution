28f88305c1decc59fcad7e6e31ad122aed8b060a
Arman Simonyan <51209290+rmnsimonyan@users.noreply.github.com>
2021-06-18 02:05:17 +0200

    Update 10xGenmtx_and_tsv_to_csv.sh

--- 10xGenmtx_and_tsv_to_csv.sh
+++ 10xGenmtx_and_tsv_to_csv.sh
@@ -1,12 +1,12 @@
-# This script transforms 10x Genomics scRNA-seq output files (barcodes.tsv.gz, features.tsv.gz, matrix.mtx.gz) into matrix.csv file
+# This script transforms 10x Genomics scRNA-seq output files (barcodes.tsv.gz, features.tsv.gz, matrix.mtx.gz) into matrix.csv file (use zcat for gziped files and cat for not gziped files)
 
 # Print line number along with contents of barcodes.tsv.gz and genes.tsv.gz
-zcat < barcodes.tsv.gz | awk -F "\t" 'BEGIN { OFS = "," }; {print NR,$1}' | sort -t, -k 1b,1 > numbered_barcodes.csv
-zcat < features.tsv.gz | awk -F "\t" 'BEGIN { OFS = "," }; {print NR,$1,$2,$3}' | sort -t, -k 1b,1 > numbered_features.csv
+cat < barcodes.tsv | awk -F "\t" 'BEGIN { OFS = "," }; {print NR,$1}' | sort -t, -k 1b,1 > numbered_barcodes.csv
+cat < features.tsv | awk -F "\t" 'BEGIN { OFS = "," }; {print NR,$1,$2,$3}' | sort -t, -k 1b,1 > numbered_features.csv
 
 # Skip the header lines and sort matrix.mtx.gz
-zcat < matrix.mtx.gz | tail -n +4 | awk -F " " 'BEGIN { OFS = "," }; {print $1,$2,$3}' | sort -t, -k 1b,1 > feature_sorted_matrix.csv
-zcat < matrix.mtx.gz | tail -n +4 | awk -F " " 'BEGIN { OFS = "," }; {print $1,$2,$3}' | sort -t, -k 2b,2 > barcode_sorted_matrix.csv
+cat < matrix.mtx | tail -n +4 | awk -F " " 'BEGIN { OFS = "," }; {print $1,$2,$3}' | sort -t, -k 1b,1 > feature_sorted_matrix.csv
+cat < matrix.mtx | tail -n +4 | awk -F " " 'BEGIN { OFS = "," }; {print $1,$2,$3}' | sort -t, -k 2b,2 > barcode_sorted_matrix.csv
 
 # Use join to replace line number with barcodes and genes
 join -t, -1 1 -2 1 numbered_features.csv feature_sorted_matrix.csv | cut -d, -f 2,3,4,5,6 | sort -t, -k 4b,4 | join -t, -1 1 -2 4 numbered_barcodes.csv - | cut -d, -f 2,3,4,5,6 > final_matrix.csv
