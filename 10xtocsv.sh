9ca7f8881bdbf9562d841b2de11f15decc837db4
Arman Simonyan <51209290+rmnsimonyan@users.noreply.github.com>
2021-06-18 02:23:25 +0200

    Update 10xtocsv.sh

--- 10xtocsv.sh
+++ 10xtocsv.sh
@@ -1,5 +1,8 @@
 # Copy the script into the directory of data and run from there
 dir=$1
-cp 10xGenmtx_and_tsv_to_csv.sh $dir
-cd $dir
+cd data
+unzip $dir.zip
+cd ..
+cp 10xGenmtx_and_tsv_to_csv.sh data/$dir
+cd data/$dir
 sh 10xGenmtx_and_tsv_to_csv.sh
